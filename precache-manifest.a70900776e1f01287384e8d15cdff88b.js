51360f55a8f631ea28749fb382fffb7fa1190d9d
nguyenviettungg <nguyentung696969@gmail.com>
2020-08-01 14:16:00 +0700

    Updates

---
+++ precache-manifest.a70900776e1f01287384e8d15cdff88b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a32ad38c3428097008a36722f40f7e83",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "bc5c3452bdf87baa6a95",
+    "url": "/monsters-rolodex/static/css/main.0b401bc9.chunk.css"
+  },
+  {
+    "revision": "ac9375f00dc620d7ca98",
+    "url": "/monsters-rolodex/static/js/2.9a69d710.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.9a69d710.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc5c3452bdf87baa6a95",
+    "url": "/monsters-rolodex/static/js/main.b7eed0c0.chunk.js"
+  },
+  {
+    "revision": "5f64a3f135fa4288aa58",
+    "url": "/monsters-rolodex/static/js/runtime-main.ca4cfb03.js"
+  }
+]);
